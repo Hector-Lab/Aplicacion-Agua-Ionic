ad5a1e1c84aa3de1138f4f6790f51f2b2b642cd3
Hector-Lab <56307732+Hector-Lab@users.noreply.github.com>
2022-02-07 00:27:59 -0600

    Prueba de seleccion de anios y fechas

--- src/pages/buscar-corte/buscar-corte.tsx
+++ src/pages/buscar-corte/buscar-corte.tsx
@@ -1,17 +1,45 @@
-import { IonButton, IonButtons, IonCard, IonCardHeader, IonCol, IonContent, IonDatetime, IonHeader, IonLabel, IonMenuButton, IonModal, IonPage, IonRippleEffect, IonRow, IonText, IonTitle, IonToolbar, useIonViewDidEnter, useIonViewWillEnter } from "@ionic/react";
-import { timeEnd } from "node:console";
+import { IonButtons, 
+    IonCard, 
+    IonCardHeader, 
+    IonCol, 
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonRippleEffect, 
+    IonRow, 
+    IonText, 
+    IonTitle, 
+    IonToolbar, 
+    useIonViewDidEnter, 
+    useIonViewWillEnter,
+    IonSelect,
+    IonSelectOption } from "@ionic/react";
 import { useEffect, useState } from "react";
+import { generarFechas } from '../../utilities'; 
 import MenuLeft from '../../components/left-menu';
 import './buscar-corte.css'
 const BuscarCorte: React.FC = () => {
     const [activarMenu , setActivarMenu ] = useState(true);
     const [fechaActual, setFechaActual ] = useState(String);
+    const [ anios, setAnios ] = useState<any[]>([]);
+    const [ meses, setMeses ] = useState<any[]>([]);
+    const [ anioSeleccionado, setAnioSeleccionado ] = useState(11);
+    const [ mesSelecionado, setMesSeleccionado ] = useState(Number);
     const fecha = new Date();
     useIonViewWillEnter(()=>{setActivarMenu(false)});
     useIonViewDidEnter(()=>{setActivarMenu(true)});
     useEffect(()=>{
         setFechaActual(fecha.getFullYear() + "-" + (fecha.getMonth() + 1) + "-" + (fecha.getDate()  + 1 ));
-    },[])
+        preparaPantalla(String(fecha.getFullYear()));
+    },[]);
+    const preparaPantalla = async (anio:string) =>{
+        let aniosMeses = await generarFechas(fecha.getFullYear());
+        console.log(aniosMeses);
+        setMeses(aniosMeses[0].Meses);
+        setAnios(aniosMeses[1].Anios);
+        setMesSeleccionado(fecha.getMonth());
+    }
     return(
         <IonPage>
             {
@@ -32,16 +60,39 @@ const BuscarCorte: React.FC = () => {
                         <IonTitle className = "center" > Historial de cortes </IonTitle>
                         <br />
                         <br />
-                        <IonRow className = "bordeFecha"  >
+                        <IonRow className = "bordeFecha" >
                             <IonCol size = "6" className = "labelCalendario" >
-                                <IonText > Seleccione una fecha: </IonText>
+                                <IonText > Seleccione el año: </IonText>
                             </IonCol>    
                             <IonCol size="6" className = "ion-activatable ripple-parent">
                                 <div >
-                                    <IonDatetime value = {fechaActual} displayFormat="DD-MM-YYYY" pickerFormat="DD-MM-YYYY" cancelText="Cancelar" doneText="Aceptar" onIonChange={e => { setFechaActual(String(e.detail.value)) }} ></IonDatetime>
+                                    <IonSelect value={ anioSeleccionado }>
+                                        {
+                                            anios != null ?
+                                            anios.map((item,index)=>{
+                                                return <IonSelectOption className="selectedItemBold"  value={ item.id } key = {index} >{ item.anio }</IonSelectOption>
+                                            }) : <></>
+                                        }
+                                    </IonSelect>
                                     <IonRippleEffect></IonRippleEffect>
                                 </div>
                             </IonCol>
+                        </IonRow>
+                        <IonRow className = "bordeFecha" >
+                            <IonCol size="6" class="labelCalendario">
+                                <IonText> Seleccione mes: </IonText>
+                            </IonCol>
+                            <IonCol>
+                                <div>
+                                    <IonSelect value = { mesSelecionado } >
+                                        {
+                                            meses.map((mes,item)=>{
+                                                return <IonSelectOption className="selectedItemBold" value={mes.id} key = {mes.id} >{ mes.mes }</IonSelectOption>
+                                            })
+                                        }
+                                    </IonSelect>
+                                </div>
+                            </IonCol> 
                         </IonRow>
                     </IonCardHeader>
                 </IonCard>
